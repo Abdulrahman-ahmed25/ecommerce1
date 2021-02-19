63249e833255c0176179ba0146e198a92444048c
Abdulrahman Ahmed <76537136+Abdulrahman-ahmed25@users.noreply.github.com>
2021-02-19 23:52:26 +0200

    Update settings.py

--- ecommerce1/settings.py
+++ ecommerce1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&h-)31c)i4z8s)w2ic6c#l-ebq&#5j8f)=y_csrargbfaugqf5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
